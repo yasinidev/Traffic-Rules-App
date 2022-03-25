eeb85a1cae4fe7c62da312efc61c805c0141cf83
yasiniweb <yasini4real@gmail.com>
2022-03-25 23:25:56 +0200

    Road-Rules App

---
+++ app/src/main/java/com/example/amategekoyumuhanda101/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.amategekoyumuhanda101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/amategekoyumuhanda101/QuizQuestionsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.amategekoyumuhanda101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+
+
+    }
+}
