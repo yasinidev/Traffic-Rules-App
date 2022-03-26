b64e403ed7a4a000c97729f80d66faae724e8fa9
yasiniweb <yasini4real@gmail.com>
2022-03-26 11:39:37 +0200

    Update

--- app/src/main/java/com/example/amategekoyumuhanda101/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/amategekoyumuhanda101/QuizQuestionsActivity.kt
@@ -2,18 +2,13 @@ package com.example.amategekoyumuhanda101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionsList = Constants.getQuestions()
-        Log.e("Questions Size", "${questionsList.size}")
-        for (i in questionsList) {
-            Log.e("Questions", i.question)
-        }
+
 
     }
 }
